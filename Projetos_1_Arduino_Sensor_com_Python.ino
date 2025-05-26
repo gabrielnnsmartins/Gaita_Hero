10c816d2278e2f29fce9ea5485b3a004b54561fa
Gabriel Nunes Martins <gabrielnnsmartins@hotmail.com>
2025-05-26 14:05:23 -0300

    Update Projetos_1_Arduino_Sensor_com_Python.ino
    ajustei o nome das variáveis

--- Projetos_1_Arduino_Sensor_com_Python.ino
+++ Projetos_1_Arduino_Sensor_com_Python.ino
@@ -21,31 +21,31 @@ void loop() {
   float tensaoSaida_VERDE = leituraAnalogica_VERDE * (VCC / 1023.0);
   
   float pressao_kPa_VERDE = (tensaoSaida_VERDE / VCC - 0.095) / 0.009;
-  if (1 <= pressa_kPa_VERDE <= 2);
+  if (1 <= pressao_kPa_VERDE <= 2);
     Serial.println("Verde")
 
   float tensaoSaida_VERMELHO = leituraAnalogica_VERMELHO * (VCC / 1023.0);
   float pressao_kPa_VERMELHO = (tensaoSaida_VERMELHO / VCC - 0.095) / 0.009;
-  if (1 <= pressa_kPa_VERMELHO <= 2);
+  if (1 <= pressao_kPa_VERMELHO <= 2);
     Serial.println("Vermelho")
 
   
 
   float tensaoSaida_AMARELO = leituraAnalogica_AMARELO * (VCC / 1023.0);
   float pressao_kPa_AMARELO = (tensaoSaida_AMARELO / VCC - 0.095) / 0.009;
-  if (1 <= pressa_kPa_AMARELO <= 2);
+  if (1 <= pressao_kPa_AMARELO <= 2);
     Serial.println("Amarelo")
 
 
   float tensaoSaida_AZUL = leituraAnalogica_AZUL * (VCC / 1023.0);
   float pressao_kPa_AZUL = (tensaoSaida_AZUL / VCC - 0.095) / 0.009;
-  if (1 <= pressa_kPa_AZUL <= 2);
+  if (1 <= pressao_kPa_AZUL <= 2);
     Serial.println("Azul")
 
 
   float tensaoSaida_ROXO = leituraAnalogica_ROXO * (VCC / 1023.0);
   float pressao_kPa_ROXO = (tensaoSaida_ROXO / VCC - 0.095) / 0.009;
-  if (1 <= pressa_kPa_ROXO <= 2);
+  if (1 <= pressao_kPa_ROXO <= 2);
     Serial.println("Roxo")
 
   
