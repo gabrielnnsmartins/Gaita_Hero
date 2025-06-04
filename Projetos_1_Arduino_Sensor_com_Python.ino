af353e4db13c41491140e914ea3c80011393d2e1
Gabriel Nunes Martins <gabrielnnsmartins@hotmail.com>
2025-06-04 17:11:12 -0300

    Update Projetos_1_Arduino_Sensor_com_Python.ino
    essa mudança foi feita após mudarmos o sensor, a limitação desse sensor é que só consegue conectar-se as portas analógicas A4 e A5, nem conseguindo mudar de endereço.

--- Projetos_1_Arduino_Sensor_com_Python.ino
+++ Projetos_1_Arduino_Sensor_com_Python.ino
@@ -1,56 +1,28 @@
-const int SENSOR_PIN_VERDE = A0;
-const int SENSOR_PIN_VERMELHO=A1;
-const int SENSOR_PIN_AMARELO=A2;
-const int SENSOR_PIN_AZUL=A3;
-const int SENSOR_PIN_ROXO=A4;
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 
-const float VCC = 5.0;
+Adafruit_BMP085 bmp;
+
+int ultimovalor = 0; 
 
 void setup() {
   Serial.begin(9600);
+  if (!bmp.begin()) {
+    Serial.println("Sensor BMP180 não encontrado, verifique as conexões!");
+    while (1); 
+  }
 }
 
 void loop() {
-  int leituraAnalogica_VERDE = analogRead(SENSOR_PIN_VERDE);
-  int leituraAnalogica_VERMELHO = analogRead(SENSOR_PIN_VERMELHO);
-  int leituraAnalogica_AMARELO = analogRead(SENSOR_PIN_AMARELO);
-  int leituraAnalogica_AZUL = analogRead(SENSOR_PIN_AZUL);
-  int leituraAnalogica_ROXO = analogRead(SENSOR_PIN_ROXO);
+  int valoratual = bmp.readPressure();
   
-  
-  float tensaoSaida_VERDE = leituraAnalogica_VERDE * (VCC / 1023.0);
-  
-  float pressao_kPa_VERDE = (tensaoSaida_VERDE / VCC - 0.095) / 0.009;
-  if (1 <= pressao_kPa_VERDE <= 2);
-    Serial.println("Verde")
+  Serial.print(valoratual);
+  Serial.println(" Pa");
 
-  float tensaoSaida_VERMELHO = leituraAnalogica_VERMELHO * (VCC / 1023.0);
-  float pressao_kPa_VERMELHO = (tensaoSaida_VERMELHO / VCC - 0.095) / 0.009;
-  if (1 <= pressao_kPa_VERMELHO <= 2);
-    Serial.println("Vermelho")
+  if (valoratual > ultimovalor + 10) {  
+    Serial.println("Vermelho");
+  }
 
-  
-
-  float tensaoSaida_AMARELO = leituraAnalogica_AMARELO * (VCC / 1023.0);
-  float pressao_kPa_AMARELO = (tensaoSaida_AMARELO / VCC - 0.095) / 0.009;
-  if (1 <= pressao_kPa_AMARELO <= 2);
-    Serial.println("Amarelo")
-
-
-  float tensaoSaida_AZUL = leituraAnalogica_AZUL * (VCC / 1023.0);
-  float pressao_kPa_AZUL = (tensaoSaida_AZUL / VCC - 0.095) / 0.009;
-  if (1 <= pressao_kPa_AZUL <= 2);
-    Serial.println("Azul")
-
-
-  float tensaoSaida_ROXO = leituraAnalogica_ROXO * (VCC / 1023.0);
-  float pressao_kPa_ROXO = (tensaoSaida_ROXO / VCC - 0.095) / 0.009;
-  if (1 <= pressao_kPa_ROXO <= 2);
-    Serial.println("Roxo")
-
-  
-  delay(2)
-
-  
+  ultimovalor = valoratual;
 
 }
